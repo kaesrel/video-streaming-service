c38eb08645b766ff077b3d2c9935400b7b058ce1
kaesrel <chanathip.thu@ku.th>
2022-03-19 22:55:18 +0700

    Add history, recommendations, and messaging queues

--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -1,91 +1,93 @@
 const express = require("express");
-const http = require("http");
-const mongodb = require("mongodb");
+const fs = require("fs");
+const amqp = require('amqplib');
 
-const app = express();
+if (!process.env.RABBIT) {
+    throw new Error("Please specify the name of the RabbitMQ host using environment variable RABBIT");
+}
+
+const RABBIT = process.env.RABBIT;
 
 //
-// Throws an error if the any required environment variables are missing.
+// Connect to the RabbitMQ server.
 //
+function connectRabbit() {
 
-if (!process.env.PORT) {
-    throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
-}
+    console.log(`Connecting to RabbitMQ server at ${RABBIT}.`);
 
-if (!process.env.VIDEO_STORAGE_HOST) {
-    throw new Error("Please specify the host name for the video storage microservice in variable VIDEO_STORAGE_HOST.");
-}
+    return amqp.connect(RABBIT) // Connect to the RabbitMQ server.
+        .then(connection => {
+            console.log("Connected to RabbitMQ.");
 
-if (!process.env.VIDEO_STORAGE_PORT) {
-    throw new Error("Please specify the port number for the video storage microservice in variable VIDEO_STORAGE_PORT.");
-}
-
-if (!process.env.DBHOST) {
-    throw new Error("Please specify the databse host using environment variable DBHOST.");
-}
-
-if (!process.env.DBNAME) {
-    throw new Error("Please specify the name of the database using environment variable DBNAME");
+            return connection.createChannel() // Create a RabbitMQ messaging channel.
+                .then(messageChannel => {
+                    return messageChannel.assertExchange("viewed", "fanout") // Assert that we have a "viewed" exchange.
+                        .then(() => {
+                            return messageChannel;
+                        });
+                });
+        });
 }
 
 //
-// Extracts environment variables to globals for convenience.
+// Send the "viewed" to the history microservice.
 //
+function sendViewedMessage(messageChannel, videoPath) {
+    console.log(`Publishing message on "viewed" exchange.`);
+        
+    const msg = { videoPath: videoPath };
+    const jsonMsg = JSON.stringify(msg);
+    messageChannel.publish("viewed", "", Buffer.from(jsonMsg)); // Publish message to the "viewed" exchange.
+}
 
-const PORT = process.env.PORT;
-const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
-const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT);
-const DBHOST = process.env.DBHOST;
-const DBNAME = process.env.DBNAME;
+//
+// Setup event handlers.
+//
+function setupHandlers(app, messageChannel) {
+    app.get("/video", (req, res) => { // Route for streaming video.
 
-console.log(`Forwarding video requests to ${VIDEO_STORAGE_HOST}:${VIDEO_STORAGE_PORT}.`);
+        const videoPath = "./videos/SampleVideo_1280x720_1mb.mp4";
+        fs.stat(videoPath, (err, stats) => {
+            if (err) {
+                console.error("An error occurred ");
+                res.sendStatus(500);
+                return;
+            }
+    
+            res.writeHead(200, {
+                "Content-Length": stats.size,
+                "Content-Type": "video/mp4",
+            });
+    
+            fs.createReadStream(videoPath).pipe(res);
 
+            sendViewedMessage(messageChannel, videoPath); // Send message to "history" microservice that this video has been "viewed".
+        });
+    });
+}
+
+//
+// Start the HTTP server.
+//
+function startHttpServer(messageChannel) {
+    return new Promise(resolve => { // Wrap in a promise so we can be notified when the server has started.
+        const app = express();
+        setupHandlers(app, messageChannel);
+
+        const port = process.env.PORT && parseInt(process.env.PORT) || 3000;
+        app.listen(port, () => {
+            resolve(); // HTTP server is listening, resolve the promise.
+        });
+    });
+}
+
+//
+// Application entry point.
+//
 function main() {
-    return mongodb.MongoClient.connect(DBHOST) // Connect to the database.
-        .then(client => {
-            const db = client.db(DBNAME);
-            const videosCollection = db.collection("videos");
-        
-            app.get("/video", (req, res) => {
-                const videoId = new mongodb.ObjectID(req.query.id);
-                videosCollection.findOne({ _id: videoId })
-                    .then(videoRecord => {
-                        if (!videoRecord) {
-                            res.sendStatus(404);
-                            return;
-                        }
-
-                        console.log(`Translated id ${videoId} to path ${videoRecord.videoPath}.`);
-        
-                        const forwardRequest = http.request( // Forward the request to the video storage microservice.
-                            {
-                                host: VIDEO_STORAGE_HOST,
-                                port: VIDEO_STORAGE_PORT,
-                                path:`/video?path=${videoRecord.videoPath}`, // Video path now retrieved from the database.
-                                method: 'GET',
-                                headers: req.headers
-                            }, 
-                            forwardResponse => {
-                                res.writeHeader(forwardResponse.statusCode, forwardResponse.headers);
-                                forwardResponse.pipe(res);
-                            }
-                        );
-                        
-                        req.pipe(forwardRequest);
-                    })
-                    .catch(err => {
-                        console.error("Database query failed.");
-                        console.error(err && err.stack || err);
-                        res.sendStatus(500);
-                    });
-            });
-
-            //
-            // Starts the HTTP server.
-            //
-            app.listen(PORT, () => {
-                console.log(`Microservice listening, please load the data file db-fixture/videos.json into your database before testing this microservice.`);
-            });
+    return connectRabbit()                          // Connect to RabbitMQ...
+        .then(messageChannel => {                   // then...
+            return startHttpServer(messageChannel); // start the HTTP server.
         });
 }
 
